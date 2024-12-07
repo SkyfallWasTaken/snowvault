7627993e75c1e5e5abca8167d8c0521a1c2d173e
Mahad Kalam <mahadkalam1234@gmail.com>
2024-12-07 21:04:39 +0000

    remove gui + use error type

--- crates/snowvault/src/lib.rs
+++ crates/snowvault/src/lib.rs
@@ -9,7 +9,6 @@ use aes_gcm::{
     Aes256Gcm, Nonce,
 };
 use argon2::Argon2;
-use eyre::{eyre, Context, Result};
 use rand_chacha::{
     rand_core::{RngCore, SeedableRng},
     ChaCha20Rng,
@@ -17,6 +16,9 @@ use rand_chacha::{
 use secrecy::{ExposeSecret, ExposeSecretMut, SecretSlice, SecretString};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
+
+mod error;
+pub use error::Error;
 
 const SALT_SIZE: usize = 16; // As recommended by the Argon2 docs
 const KEY_SIZE: usize = 32; // We use AES-256, so we need a 256-bit key (32 bytes)
@@ -65,7 +67,7 @@ pub struct VaultMeta {
 }
 
 impl Vault {
-    pub fn new_from_password(path: PathBuf, password: &SecretString) -> Result<Self> {
+    pub fn new_from_password(path: PathBuf, password: &SecretString) -> Result<Self, Error> {
         let mut salt = [0u8; SALT_SIZE];
         let mut rng = ChaCha20Rng::from_entropy();
         rng.fill_bytes(&mut salt);
@@ -94,42 +96,39 @@ impl Vault {
         Ok(vault)
     }
 
-    pub fn load_from_file(path: &Path, password: &SecretString) -> Result<Self> {
-        let mut file = File::open(path).wrap_err("failed to open archive file")?;
+    pub fn load_from_file(path: &Path, password: &SecretString) -> Result<Self, Error> {
+        let mut file = File::open(path)?;
         let mut vault_contents = String::new();
-        file.read_to_string(&mut vault_contents)
-            .wrap_err("failed to read archive file")?;
+        file.read_to_string(&mut vault_contents)?;
 
         let mut vault: Self = toml::from_str(&vault_contents)?;
-        let salt = hex::decode(vault.meta.salt.clone()).wrap_err("failed to decode salt")?;
+        let salt = hex::decode(vault.meta.salt.clone())?;
 
         let output_key_material = generate_master_key(password, &salt)?;
         if output_key_material.expose_secret().len() != 32 {
-            return Err(eyre!("invalid key length"));
+            return Err(Error::InvalidKeyLength);
         }
         vault.meta.master_key = Some(output_key_material.clone());
 
         verify_master_key(&vault, &output_key_material)?;
 
         let cipher = Aes256Gcm::new_from_slice(output_key_material.expose_secret())
-            .map_err(|_| eyre!("invalid key length"))?;
+            .map_err(|_| Error::InvalidKeyLength)?;
 
         decrypt_entries(&mut vault, &cipher)?;
 
         Ok(vault)
     }
 
-    pub fn add_entry(&mut self, entry: VaultEntry, key: &SecretSlice<u8>) -> Result<()> {
+    pub fn add_entry(&mut self, entry: VaultEntry, key: &SecretSlice<u8>) -> Result<(), Error> {
         let nonce = {
             let mut rng = ChaCha20Rng::from_entropy();
             Aes256Gcm::generate_nonce(&mut rng)
         };
-        let cipher = Aes256Gcm::new_from_slice(key.expose_secret())
-            .map_err(|_| eyre!("invalid key length"))?;
+        let cipher =
+            Aes256Gcm::new_from_slice(key.expose_secret()).map_err(|_| Error::InvalidKeyLength)?;
         let plaintext = toml::to_string(&entry)?;
-        let ciphertext = cipher
-            .encrypt(&nonce, plaintext.as_bytes())
-            .map_err(|e| eyre!("encryption error: {}", e))?;
+        let ciphertext = cipher.encrypt(&nonce, plaintext.as_bytes())?;
         let enc_entry = EncVaultEntry {
             nonce: hex::encode(nonce),
             encoded_ciphertext: hex::encode(ciphertext),
@@ -144,7 +143,7 @@ impl Vault {
         self.entries.remove(index);
     }
 
-    pub fn save(&self) -> Result<()> {
+    pub fn save(&self) -> Result<(), Error> {
         // Everything's encrypted already, so this isn't an issue
         let toml: String = toml::to_string(self)?;
         std::fs::write(&self.path, toml)?;
@@ -152,20 +151,18 @@ impl Vault {
     }
 }
 
-fn generate_master_key(password: &SecretString, salt: &[u8]) -> Result<SecretSlice<u8>> {
+fn generate_master_key(password: &SecretString, salt: &[u8]) -> Result<SecretSlice<u8>, Error> {
     let mut output_key_material = SecretSlice::new(Box::new([0u8; KEY_SIZE]));
-    Argon2::default()
-        .hash_password_into(
-            password.expose_secret().as_bytes(),
-            salt,
-            output_key_material.expose_secret_mut(),
-        )
-        .map_err(|_| eyre!("failed to generate master key when opening archive"))?;
+    Argon2::default().hash_password_into(
+        password.expose_secret().as_bytes(),
+        salt,
+        output_key_material.expose_secret_mut(),
+    )?;
     debug_assert_eq!(output_key_material.expose_secret().len(), KEY_SIZE);
     Ok(output_key_material)
 }
 
-fn verify_master_key(vault: &Vault, output_key_material: &SecretSlice<u8>) -> Result<()> {
+fn verify_master_key(vault: &Vault, output_key_material: &SecretSlice<u8>) -> Result<(), Error> {
     let master_key_hash = hex::encode(
         Sha256::new()
             .chain_update(output_key_material.expose_secret())
@@ -174,21 +171,21 @@ fn verify_master_key(vault: &Vault, output_key_material: &SecretSlice<u8>) -> Re
     );
 
     if master_key_hash != vault.meta.master_key_hash {
-        return Err(eyre!("invalid password"));
+        return Err(Error::InvalidPassword);
     }
     Ok(())
 }
 
-fn decrypt_entries(vault: &mut Vault, cipher: &Aes256Gcm) -> Result<()> {
+fn decrypt_entries(vault: &mut Vault, cipher: &Aes256Gcm) -> Result<(), Error> {
     for entry in &vault.enc_entries {
         let nonce_vec = hex::decode(entry.nonce.clone())?;
         let nonce = Nonce::from_slice(&nonce_vec);
         let ciphertext = hex::decode(&entry.encoded_ciphertext)?;
         let plaintext = cipher
             .decrypt(nonce, ciphertext.as_slice())
-            .map_err(|e| eyre!("decryption error: {}", e))?;
+            .map_err(|_| Error::AesGcmError)?;
 
-        let entry_str = std::str::from_utf8(&plaintext)?;
+        let entry_str = std::str::from_utf8(&plaintext).unwrap();
         let entry: VaultEntry = toml::from_str(entry_str)?;
         vault.entries.push(entry);
     }
